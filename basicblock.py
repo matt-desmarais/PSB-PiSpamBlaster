2dbb2ec3c3d6cb7a719d4d840b2e5d70f7389883
Matt <matt.desmarais78@gmail.com>
2019-11-09 20:21:15 -0500

    Update basicblock.py

--- basicblock.py
+++ basicblock.py
@@ -1,8 +1,6 @@
 import serial
 import time
-from squid import *
 ser = serial.Serial('/dev/ttyACM0')
-rgb = Squid(16, 20, 21)
 ser.flushInput()
 print(ser.name) 
 #test modem
@@ -18,21 +16,15 @@ ser.write('ATS11=50'+'\r\n')
 
 while True:
     try:
-        rgb.set_color(OFF)
         #get serial input
         line = ser.readline()
         with open("modemOuput.txt", "a") as modemOutput:
             modemOutput.write(str(line)+'|')
         print(line)
-        if("RING" in str(line)):
-            rgb.set_color(GREEN)
         #check for spam call or blocked caller id
         if("NAME = SPAM?" in str(line) or "NMBR = P" in str(line)):
-            rgb.set_color(RED)
             #answer modem
             ser.write('ATA'+'\r\n')
-            #answer dial
-            #ser.write('ATDT1234567890987654321'+'\r\n')
             time.sleep(12)
             #hangup
             ser.write('ATH0'+'\r\n')
